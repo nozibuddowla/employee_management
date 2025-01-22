ecaa1142e0e22afc7ce1892ce2bf4816b228c0a3
nozibuddowla <nozibuddowla@gmail.com>
2025-01-23 05:01:16 +0600

    add unique employee profile feature

--- employees/views.py
+++ employees/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render, redirect
 from .forms import EmployeesForm
+from django.db import IntegrityError
 
 # Create your views here.
 def add_employee(request):
     if request.method == 'POST':
         form = EmployeesForm(request.POST)
         if form.is_valid():
-            form.save()
-            return redirect('index')
+            try:
+                form.save()
+                return redirect('index')
+            except IntegrityError:
+                form.add_error('phone_number', 'This Phone number already exists')
+                form.add_error('email', 'This Email already exists')
     else:
         form = EmployeesForm()
 
